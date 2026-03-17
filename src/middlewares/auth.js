188b78116813b196d0cfef0853411adbf9651ece
rahul-yadav695 <officialrahulyadav961@gmail.com>
2026-03-17 16:23:20 +0530

    somethong changes

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,20 +1,29 @@
 const jwt = require("jsonwebtoken")
+const tokenBlackList = require("../module/blacklist")
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
     const token = req.cookies.token
 
     if (!token) {
         return res.status(401).json({ message: "token not provided" })
     }
+
+    const istokenblacklisted = await tokenBlackList.findOne({ token })
+
+    if (istokenblacklisted) {
+        return res.status(401).json({ message: "token is blacklisted" })
+    }
+
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
         req.user = decoded
-        next()
+        next() 
 
     } catch (err) {
         return res.send({ status: 0, message: "something went wrong try again", err })
     }
 }
+
 
 module.exports = { authMiddleware }
