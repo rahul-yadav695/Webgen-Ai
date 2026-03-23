ce1885a5f00aaf9b63896ed505d1c811b8af8d7a
rahul-yadav695 <officialrahulyadav961@gmail.com>
2026-03-23 16:51:24 +0530

    move to src folder

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const connectDB = require('../src/config/data')
+// const connectDB = require('../src/config/data')
 const cookieParser = require("cookie-parser")
 
 const app = express()
@@ -9,9 +9,10 @@ const port = 3000
 app.use(express.json())
 app.use(cookieParser())
 
-connectDB()
+
 
 const authRouter = require('../src/routes/user')
+const { connect } = require('mongoose')
 app.use('/api', authRouter)
 
 app.listen(port, () => {
