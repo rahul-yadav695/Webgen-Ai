7fe57841471a57e4e09238e0b945eb6ac60e68b0
rahul-yadav695 <officialrahulyadav961@gmail.com>
2026-03-24 12:35:29 +0530

    auth apis

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,33 +1,52 @@
 import axios from "axios";
 
-const register = async ({ username, email, password }) => {
-    try {
-        const response = await axios.post('http://localhost:3000/api/register', {
-            username, email, password
-        }, {
-            withCredentials: true
-        })
-        return response.data
-    } catch (err) {
-        console.log('Registration error:', err)
-    }
-}
+const API_URL = axios.create({
+  baseURL: "http://localhost:3000/api",
+  withCredentials: true,
+});
 
+export const register = async ({ username, email, password }) => {
+  try {
+    const response = await API_URL.post("/register", {
+      username,
+      email,
+      password,
+    });
+    return response.data;
+  } catch (err) {
+    console.log("Registration error:", err);
+    throw err;
+  }
+};
 
-const login = async ({ email, password }) => {
-    try {
-        const response = await axios.post('http://localhost:3000/api/login', {
-            email, password
-        }, {
-            withCredentials: true
-        })
-        return response.data
-    } catch (err) {
-        console.log('Login error:', err)
-    }
-}
+export const login = async ({ email, password }) => {
+  try {
+    const response = await API_URL.post("/login", {
+      email,
+      password,
+    });
+    return response.data;
+  } catch (err) {
+    console.log("Login error:", err);
+    throw err;
+  }
+};
 
-module.exports = {
-    register,
-    login
-}
+export const logout = async () => {
+  try {
+    await API_URL.post("/logout");
+  } catch (err) {
+    console.log("Logout error:", err);
+    throw err;
+  }
+};
+
+export const getCurrentUser = async () => {
+  try {
+    const response = await API_URL.get("/get-me");
+    return response.data;
+  } catch (err) {
+    console.log("Get current user error:", err);
+    throw err;
+  }
+};
